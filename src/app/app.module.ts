7cffa42ea7cc176e2351291a6057639a79e7e006
đinh văn thành <dinhvanthanh1995@outlook.com>
2017-09-29 20:50:12 +0700

    update UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Templates/header/header.component'; 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule ,BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
